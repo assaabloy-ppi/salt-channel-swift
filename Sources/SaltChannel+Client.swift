ff3a3f6d60af42dcded4090c5f1fa5943b7865ed
Kenneth Pernyer <kenneth@pernyer.se>
2017-10-16 17:11:55 +0200

    Split Client and Host of Salt into their own files

--- Sources/SaltChannel+Client.swift
+++ Sources/SaltChannel+Client.swift
@@ -13,13 +13,12 @@ extension SaltChannel: Client {
     public func writeM1(time: TimeInterval, myEncPub: Data, serverSignPub: Data? = nil) throws -> Data {
         
         let serverSignKeys = (serverSignPub != nil)
-        let header = createHeader(from: PacketType.M1, first: serverSignKeys)
+        let header = createHeader(from: PacketType.m1, first: serverSignKeys)
         
         // TODO: better toBytes for Double
         var m1 = Constants.protocolId + header + packBytes(UInt64(time), parts: 4) + myEncPub
         if let serverKeys = serverSignPub {
-            os_log("Client: Using Server Sign PubKeys %{public}s",
-                   log: log, type: .debug, serverKeys as CVarArg)
+            os_log("Client: Using Server Sign PubKeys %{public}s", log: log, type: .debug, serverKeys as CVarArg)
             m1 += serverKeys
         } else {
             // TODO
@@ -44,7 +43,7 @@ extension SaltChannel: Client {
             throw ChannelError.errorInMessage(reason:
                 "NoSuchMessage and Last must both be set or none of them")
         }
-        guard type == PacketType.M2 else {
+        guard type == PacketType.m2 else {
             throw ChannelError.badMessageType(reason: "Expected M2 Header ")
         }
         
@@ -83,7 +82,7 @@ extension SaltChannel: Client {
        let header = data[..<2]
         
         let (type, _, _) = readHeader(from: header)
-        guard type == PacketType.M3 else {
+        guard type == PacketType.m3 else {
             throw ChannelError.badMessageType(reason: "Expected M3 Header")
         }
         
@@ -108,7 +107,7 @@ extension SaltChannel: Client {
      ##M4## is sent to the server encrypted
      */
     public func writeM4(time: TimeInterval, clientSignSec: Data, clientSignPub: Data, m1Hash: Data, m2Hash: Data) throws -> Data {
-        let header = createHeader(from: PacketType.M4)
+        let header = createHeader(from: PacketType.m4)
         let signedMessage = Constants.clientprefix + m1Hash + m2Hash
         guard let signature = createSignature(message: signedMessage, signSec: clientSignSec) else {
             throw ChannelError.couldNotCreateSignature
@@ -136,7 +135,7 @@ extension SaltChannel: Client {
                 "NoSuchMessage and Last must both be set or none of them")
         }
     
-        guard type == PacketType.A2 else {
+        guard type == PacketType.a2 else {
             throw ChannelError.badMessageType(reason: "Expected A2 message header")
         }
         
@@ -151,102 +150,7 @@ extension SaltChannel: Client {
     }
 }
 
-extension SaltChannel: Peer {
-    /**
-     */
-    func writeApp(time: TimeInterval, message: Data) -> Data {
-        let header = createHeader(from: PacketType.App)
-        return header + packBytes(UInt64(time), parts: 4) + message
-    }
-    
-    /**
-     */
-    public func readApp(data: Data) throws -> (time: TimeInterval, message: Data) {
-        let header = data[..<2]
-        let (type, _, _) = readHeader(from: header)
-        guard  type == PacketType.App else {
-            throw ChannelError.badMessageType(reason: "Expected App message header")
-        }
-        
-        // TODO: better unpack for Time
-        let (time, _) = unpackInteger(data.subdata(in: 2 ..< 6), count: 4)
-        let message = data.subdata(in: 6 ..< data.endIndex)
-        return (Double(time), message)
-    }
-    
-    /**
-     ##MultiApp## is multiple messages batched together in the application layer protocol
-     */
-    func writeMultiApp(data: Data) throws -> (time: TimeInterval, message: Data) {
-        let header = createHeader(from: PacketType.MultiApp)
-        let time = self.session!.time
-        let message = header + packBytes(UInt64(time), parts: 4)
-        return (time, message)
-    }
-}
-
-extension SaltChannel: Host {
-    ///- Host --
-    
-    /**
-     ##M1## is sent to the server in plain
-     */
-    public func readM1(data: Data) throws -> (time: TimeInterval, remoteEncPub: Data, hash: Data) {
-        os_log("Host: unpack M1", log: log, type: .debug)
-        
-        let hash = sodium.genericHash.hashSha512(data: data)
-        let protocolId = data[..<4]
-        guard protocolId ==  Constants.protocolId else {
-            throw ChannelError.badMessageType(reason: "Expected M1 Header")
-        }
-        
-        let header = data[4..<6]
-        let (type, _, last) = readHeader(from: header)
-        guard type == PacketType.M1 else {
-            throw ChannelError.badMessageType(reason: "Expected M1 Header")
-        }
-        
-        if last {
-            guard let session = session else {
-                throw ChannelError.setupNotDone(reason: "Host: No session object in M1")
-            }
-            session.lastMessageReceived = true
-        }
-        
-        // TODO: better unpack for Integer and convert to Double
-        let (time, _) = unpackInteger(data.subdata(in: 2 ..< 6), count: 4)
-        let remoteEncPub = data.subdata(in: 6 ..< data.endIndex)
-        guard remoteEncPub.count == 32 else {
-            throw ChannelError.errorInMessage(reason: "Size of Messsage is wrong. \(time)")
-        }
-        
-        let realtime = TimeInterval(time)
-        os_log("M1 returning. Time= %{public}s", log: log, type: .debug, realtime)
-        return (realtime, remoteEncPub, hash)
-    }
-    
-    func writeM2(time: TimeInterval, myEncPub: Data) throws -> Data {
-        <#code#>
-    }
-    
-    func writeM3(time: TimeInterval, myEncPub: Data) throws -> Data {
-        <#code#>
-    }
-    
-    func readM4(data: Data) throws -> (time: TimeInterval, message: Data) {
-        <#code#>
-    }
-    
-    func writeA2(time: TimeInterval, message: Data) -> Data {
-        <#code#>
-    }
-    
-    func readA1(data: Data) throws -> (time: TimeInterval, message: Data) {
-        <#code#>
-    }
-}
-
-public func extractProtocols(data: Data) throws -> [String] {
+func extractProtocols(data: Data) throws -> [String] {
     var versionArray: [String] = []
     let length: Int = 10
     let size = data.count

59bf5e1672be874d21f7f58fb59acbd7c4754e51
Kenneth Pernyer <kenneth@pernyer.se>
2017-10-18 10:22:26 +0200

    Merged. Need to check with theHawkis if I got it right

--- Tests/BasicHostMock.swift
+++ Tests/BasicHostMock.swift
@@ -2,18 +2,12 @@
 //  SaltChannel-Tests
 //
 //  Created by Håkan Olsson on 2017-10-05.
-
 import XCTest
 
 @testable import SaltChannel
 
-<<<<<<< HEAD
 class BasicHostMock : ByteChannel {
     var callbacks: [(Data) -> ()] = []
-=======
-class BasicHostMock: ByteChannel, MockRunner {
-    var callbacks: [(Data) -> Void] = []
->>>>>>> d9a3a8f4b231e3913ac6c3e24a701eeb986a2340
     var writeData: [Data] = []
     var isDone: Bool = false
     let mockdata: TestDataSet
@@ -26,7 +20,6 @@ class BasicHostMock: ByteChannel, MockRunner {
         DispatchQueue.global().async { self.run() }
     }
     
-<<<<<<< HEAD
     func run() {
         if let aData = mockdata.a{
             print("Test A")
@@ -36,16 +29,11 @@ class BasicHostMock: ByteChannel, MockRunner {
         
         if let handshakeData = mockdata.handshake{
             print("Test handshake")
-=======
-    func handshake() {
-        if let handshakeData = mockdata.handshake {
->>>>>>> d9a3a8f4b231e3913ac6c3e24a701eeb986a2340
             waitForData(handshakeData.m1)
             send(handshakeData.m2)
             send(handshakeData.m3)
             waitForData(handshakeData.m4)
         }
-<<<<<<< HEAD
         
         print("Test \(mockdata.transfers.count) transfers")
         for transfer in mockdata.transfers{
@@ -54,13 +42,6 @@ class BasicHostMock: ByteChannel, MockRunner {
             }
             else {
                 send(transfer.cipher)
-=======
-        for transfer in mockdata.transfers {
-            if transfer.toHost {
-                waitForData(transfer.cipher)
-            } else {
-                send(transfer.plain)
->>>>>>> d9a3a8f4b231e3913ac6c3e24a701eeb986a2340
             }
         }
         
@@ -69,13 +50,8 @@ class BasicHostMock: ByteChannel, MockRunner {
     
     // ****** Helper functions *******
     
-<<<<<<< HEAD
     func waitForData(_ data: [Byte]){
         if WaitUntil.waitUntil(2, self.writeData.isEmpty == false) {
-=======
-    func waitForData(_ data: [Byte]) {
-        if WaitUntil.waitUntil(10, self.writeData.isEmpty == false) {
->>>>>>> d9a3a8f4b231e3913ac6c3e24a701eeb986a2340
             XCTAssertEqual(writeData.first, Data(data))
             writeData.remove(at: 0)
         }
@@ -84,8 +60,8 @@ class BasicHostMock: ByteChannel, MockRunner {
         }
     }
     
-    func send(_ data: [Byte]) {
-        for callback in callbacks {
+    func send(_ data: [Byte]){
+        for callback in callbacks{
             print("Send data")
             callback(Data(data))
         }
@@ -95,12 +71,12 @@ class BasicHostMock: ByteChannel, MockRunner {
     
     func write(_ data: [Data]) throws {
         print("Write is called in Mock")
-        for item in data {
+        for item in data{
             writeData.append(item)
         }
     }
     
-    func register(callback: @escaping (Data) -> Void, errorhandler: @escaping (Error) -> Void) {
+    func register(callback: @escaping (Data) -> (), errorhandler: @escaping (Error) -> ()) {
         print("Register is called in Mock")
         self.callbacks.append(callback)
     }
